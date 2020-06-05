6be5923415dd3bfd031860b3b24e44454c2c3cc4
Max Base <MaxBaseCode@Gmail.Com>
2020-06-06 03:56:37 +0430

    Update upgrade-repositories.php

--- upgrade-repositories.php
+++ upgrade-repositories.php
@@ -1,3 +1,24 @@
 <?php
 // Max Base
 // https://github.com/BaseMax/GitHubBackupArchiver
+include "NetPHP.php";
+$username="basemax"
+$count=260;
+$page=ceil($count / 100);
+// $path="/root/repositories";
+$path=getcwd();
+for($i=1;$i<=$page;$i++) {
+	$data=get("https://api.github.com/users/".$username."/repos?per_page=100&page=".$i);
+	$json=json_decode($data[0], true);
+	foreach($json as $item) {
+		$name=$item["name"];
+		print $name."\n";
+		if(file_exists($path."/".$name)) {
+			chdir($path."/".$name);
+			exec("git pull"); // NOTE: This repository have remote origin...
+			continue;
+		}
+		chdir($path);
+		exec("git clone ".$item["clone_url"]);
+	}
+}
